af150be07152653641fb1ed77310cdd0e203a0fd
Shubham Tukaram Patil <stp00007@gmail.com>
2024-10-27 14:39:40 +0000

    submit variable js file

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "Shubham@google.com"
+var accoundPassword ="12345"
+accountCity ="Sangli"
+//accountId = 2
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accoundPassword,accountCity])
